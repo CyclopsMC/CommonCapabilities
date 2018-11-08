1ac0302e130048bac76a4ce46429797436d70194
Ruben Taelman <rubensworks@gmail.com>
2018-11-08 18:05:33 +0100

    Don't depend on CyclopsCore in API

--- src/test/java/org/cyclops/commoncapabilities/ingredient/TestIngredientHelpers.java
+++ src/test/java/org/cyclops/commoncapabilities/ingredient/TestIngredientHelpers.java
@@ -1,9 +1,15 @@
 package org.cyclops.commoncapabilities.ingredient;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import net.minecraft.nbt.NBTTagInt;
 import org.junit.Test;
 
+import java.util.Collections;
+import java.util.Comparator;
+
 import static org.cyclops.commoncapabilities.ingredient.IngredientHelpers.compareTags;
+import static org.cyclops.commoncapabilities.api.ingredient.MixedIngredients.compareCollection;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -15,5 +21,104 @@ public class TestIngredientHelpers {
         assertThat(compareTags(null, new NBTTagInt(0)), is(-1));
         assertThat(compareTags(new NBTTagInt(0), null), is(1));
         assertThat(compareTags(new NBTTagInt(0), new NBTTagInt(0)), is(0));
+    }
+
+    @Test
+    public void testCompareCollectionEmpty() {
+        assertThat(compareCollection(Collections.emptyList(), Collections.emptyList()), is(0));
+    }
+
+    @Test
+    public void testCompareCollectionEqual() {
+        assertThat(compareCollection(
+                Lists.newArrayList(1, 2, 3),
+                Lists.newArrayList(1, 2, 3)
+        ), is(0));
+
+        assertThat(compareCollection(
+                Lists.newArrayList(1, 2, 3, 4, 5),
+                Lists.newArrayList(1, 2, 3, 4, 5)
+        ), is(0));
+
+        assertThat(compareCollection(
+                Sets.newTreeSet(Lists.newArrayList(1, 2, 3, 4, 5)),
+                Sets.newTreeSet(Lists.newArrayList(5, 2, 3, 1, 4))
+        ), is(0));
+    }
+
+    @Test
+    public void testCompareCollectionNonEqual() {
+        assertThat(compareCollection(
+                Lists.newArrayList(1, 2),
+                Lists.newArrayList(1, 2, 3)
+        ), is(-1));
+
+        assertThat(compareCollection(
+                Lists.newArrayList(1, 2, 3),
+                Lists.newArrayList(1, 2)
+        ), is(1));
+
+        assertThat(compareCollection(
+                Lists.newArrayList(2),
+                Lists.newArrayList(3)
+        ), is(-1));
+
+        assertThat(compareCollection(
+                Lists.newArrayList(3),
+                Lists.newArrayList(2)
+        ), is(1));
+    }
+
+    @Test
+    public void testCompareCollectionComparatorEmpty() {
+        assertThat(compareCollection(Collections.emptyList(), Collections.emptyList(), Comparator.<Integer>reverseOrder()), is(0));
+    }
+
+    @Test
+    public void testCompareCollectionComparatorEqual() {
+        assertThat(compareCollection(
+                Lists.newArrayList(1, 2, 3),
+                Lists.newArrayList(1, 2, 3),
+                Comparator.<Integer>reverseOrder()
+        ), is(0));
+
+        assertThat(compareCollection(
+                Lists.newArrayList(1, 2, 3, 4, 5),
+                Lists.newArrayList(1, 2, 3, 4, 5),
+                Comparator.<Integer>reverseOrder()
+        ), is(0));
+
+        assertThat(compareCollection(
+                Sets.newTreeSet(Lists.newArrayList(1, 2, 3, 4, 5)),
+                Sets.newTreeSet(Lists.newArrayList(5, 2, 3, 1, 4)),
+                Comparator.<Integer>reverseOrder()
+        ), is(0));
+    }
+
+    @Test
+    public void testCompareCollectionComparatorNonEqual() {
+        assertThat(compareCollection(
+                Lists.newArrayList(1, 2),
+                Lists.newArrayList(1, 2, 3),
+                Comparator.<Integer>reverseOrder()
+        ), is(-1));
+
+        assertThat(compareCollection(
+                Lists.newArrayList(1, 2, 3),
+                Lists.newArrayList(1, 2),
+                Comparator.<Integer>reverseOrder()
+        ), is(1));
+
+        assertThat(compareCollection(
+                Lists.newArrayList(2),
+                Lists.newArrayList(3),
+                Comparator.<Integer>reverseOrder()
+        ), is(1));
+
+        assertThat(compareCollection(
+                Lists.newArrayList(3),
+                Lists.newArrayList(2),
+                Comparator.<Integer>reverseOrder()
+        ), is(-1));
     }
 }
